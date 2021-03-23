677b632223fcb03de353227d8e2499db695b071f
Sergeytsev A <Sergeytsev A>
2021-03-23 10:45:44 +0300

    test

---
+++ TwoRegtangleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TwoRegtangleApp
+//
+//  Created by 18316409 on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var test = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
